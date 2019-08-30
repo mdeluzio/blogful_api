88643a8181e56edd088ea2eb160528e2c1d09595
Mark DeLuzio <deluziomark@gmail.com>
2019-08-30 19:47:54 -0400

    added patch method and tests

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-const ArticlesService = require('./articles-service');
+const articlesRouter = require('./articles/articles-router')
 
 const app = express();
 
@@ -17,39 +17,12 @@ app.use(morgan(morganOptions));
 app.use(helmet());
 app.use(cors());
 
-app.get('/articles', (req, res, next) => {
-    const knexInstance = req.app.get('db')
-    ArticlesService.getAllArticles(knexInstance)
-      .then(articles => {
-       res.json(articles.map(article => ({
-        id: article.id,
-        title: article.title,
-        style: article.style,
-        content: article.content,
-         date_published: new Date(article.date_published),
-        })))
-        })
-        .catch(next)
-})
+app.use('/api/articles', articlesRouter)
 
-app.get('/articles/:article_id', (req, res, next) => {
-    const knexInstance = req.app.get('db')
-    ArticlesService.getById(knexInstance, req.params.article_id)
-    .then(article => {
-        res.json({
-            id: article.id,
-            title: article.title,
-            style: article.style,
-            content: article.content,
-            date_published: new Date(article.date_published),
-        })
-    })
-})
-
-
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
-})
+app.get('/xss', (req, res) => {
+    res.cookie('secretToken', '1234567890');
+    res.sendFile(__dirname + '/xss-example.html');
+});
 
 app.use(function errorHandler(error, req, res, next) {
     let response;
--- src/articles/articles-service.js
+++ src/articles/articles-service.js
@@ -4,13 +4,13 @@ const ArticlesService = {
     },
     insertArticle(knex, newArticle) {
         return knex
-            .insert(newArticle)
-            .into('blogful_articles')
-            .returning('*')
+          .insert(newArticle)
+          .into('blogful_articles')
+          .returning('*')
             .then(rows => {
                 return rows[0]
             })
-    },
+        },
     getById(knex, id) {
         return knex.from('blogful_articles').select('*').where('id', id).first()
     },
